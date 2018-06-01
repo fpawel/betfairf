e897c813ece76bc1c9f268697c1c29dfccb06f4f
fpawel <binf@mail.ru>
2018-06-01 16:45:53 +0300

    save

---
+++ src/components/App.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import Modal from 'react-responsive-modal';
+import './custom-styling.css';
+
+export class Simple extends React.Component {
+    state = {
+        open: false,
+    };
+
+    onOpenModal = () => {
+        this.setState({ open: true });
+    };
+
+    onCloseModal = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        const { open } = this.state;
+        return (
+            <div>
+                <button onClick={this.onOpenModal}>Open simple centered modal</button>
+                <Modal open={open} onClose={this.onCloseModal} center>
+                    <h2>Simple centered modal</h2>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export class CustomStyling extends React.Component {
+    state = {
+        open: false,
+    };
+
+    onOpenModal = () => {
+        this.setState({ open: true });
+    };
+
+    onCloseModal = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        const { open } = this.state;
+        return (
+            <div className="example">
+                <h4>Custom styling</h4>
+                <button className="btn btn-action" onClick={this.onOpenModal}>
+                    Open
+                </button>{' '}
+                <a
+                    href="https://github.com/pradel/react-responsive-modal/blob/master/docs/src/examples/custom-styling.js"
+                    target="_blank"
+                >
+                    See source code
+                </a>
+                <Modal
+                    open={open}
+                    onClose={this.onCloseModal}
+                    center
+                    classNames={{ overlay: 'custom-overlay', modal: 'custom-modal' }}
+                >
+                    <h2>Modal</h2>
+                </Modal>
+            </div>
+        );
+    }
+}
+
+export const App = () => <div> <Simple />  <CustomStyling /> </div>;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mode:"development",
-    entry: ["./src/index.tsx", "./src/socket.tsx"],
+    entry: "./src/index.tsx",
     output: {
         filename: "bundle.js",
         path: __dirname + "/dist"
@@ -20,7 +20,12 @@ module.exports = {
             { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
         ]
     },
 
