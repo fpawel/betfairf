70baad5fc2dfdfb7ec9eef76eb26dca0e161b623
fpawel <binf@mail.ru>
2018-06-07 21:00:15 +0300

    save

--- src/components/Football/Football.tsx
+++ src/components/Football/Football.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {apply_games_changes, Game, GamesChanges} from "./game";
 import ReconnectingWebSocket from "reconnectingwebsocket";
 import {webSocketURL} from "../../utils/utils";
-import ReactTable, {Resize, RowInfo} from 'react-table';
+import ReactTable, {RowInfo} from 'react-table';
 import {Column, TableCellRenderer} from 'react-table';
 
 interface state {
@@ -166,8 +166,9 @@ export class Football extends React.Component<{}, state> {
     render() {
         const border = 'solid 1px #bababa';
         return <ReactTable
-            className={'-striped'} columns={columns} data={this.state.games}
-            getTrProps = {(finalState: any, rowInfo?: RowInfo, column?: undefined, instance?: any) => {
+            columns={columns}
+            data={this.state.games}
+            getTrProps = {(finalState: any, rowInfo?: RowInfo) => {
                 return {
                     style: {
                         background: (rowInfo && rowInfo.viewIndex % 2 ) ? "#e4e4e4" : undefined
@@ -175,7 +176,7 @@ export class Football extends React.Component<{}, state> {
                 };
 
             }}
-            getTdProps = {(finalState: any, rowInfo?: RowInfo, column?: Column, instance?: any) => {
+            getTdProps = {(finalState: any, rowInfo?: RowInfo, column?: Column) => {
                 return {
                     style: {
                         borderLeft:border,
@@ -187,7 +188,7 @@ export class Football extends React.Component<{}, state> {
                 };
 
             }}
-            getTheadThProps = {(finalState: any, rowInfo?: RowInfo, column?: Column, instance?: any) => {
+            getTheadThProps = {(finalState: any, rowInfo?: RowInfo, column?: Column) => {
                 return {
                     style: {
                         borderTop:border,
@@ -200,7 +201,7 @@ export class Football extends React.Component<{}, state> {
 
             }}
             defaultPageSize={35}
-            pageSizeOptions = { [20, 35, 50, 100] }
+            pageSizeOptions = { [20, 35, 50, this.state.games.length] }
             // Text
             previousText = {'Предыдущие'}
             nextText = {'Следующие'}
@@ -209,9 +210,6 @@ export class Football extends React.Component<{}, state> {
             pageText = {'Страница'}
             ofText = {'из'}
             rowsText = {'строк'}
-            onResizedChange = { (newResized: Resize[]) => {
-                //console.log(newResized);
-            } }
         />;
     }
 }
